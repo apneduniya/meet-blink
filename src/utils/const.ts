52471d1b86a70ccd834064e6a940bdd4de34fd8e
apneduniya <apneduniya.dontdisturb@gmail.com>
2024-08-03 20:55:01 +0530

    update: Another account address added as owner sol address

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -1,7 +1,7 @@
 import { PublicKey } from "@solana/web3.js";
 
 export const DEFAULT_SOL_ADDRESS: PublicKey = new PublicKey(
-  "8ueAQRiLExT7M9CNSkR11FDGbx2UyJqpSxCp1y43PWWs"
+  "AqRx3D61sna36DnhfXVXmbjuyWwuvjSpAtZfZa7pGt8R"
 );
 
 export const DEFAULT_SOL_AMOUNT: number = 0.01;

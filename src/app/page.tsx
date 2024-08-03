93b9403c5ab59c3ebf65c63bc0e3d392bb1b0d45
apneduniya <apneduniya.dontdisturb@gmail.com>
2024-08-04 00:17:39 +0530

    update: main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,9 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
-      <h1 className="text-2xl text-white">Meet link</h1>
+      <h1 className="text-2xl text-white">
+        Schedule a meet on-chain using Solana with the help of Blinks
+      </h1>
     </main>
   );
 }
